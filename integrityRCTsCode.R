8606e7456c457fa1199115611b045e3394a58d18
Ivan Ropovik <ivan.ropovik@gmail.com>
2020-11-11 23:30:42 +0100

    backup

--- integrityRCTsCode.R
+++ integrityRCTsCode.R
@@ -4,8 +4,9 @@ wosData <- read_csv("data/wosExport.csv")
 set.seed(1)
 wosData <- wosData %>%
   select(4, 3, 5, 6, 19, 18, 16, 9, 11, 40, 12 ,21, 8, 36, 1) %>%
-  mutate(paperID = sample(x = 1:nrow(wosData), size = nrow(wosData), replace = F)) %>%
-  relocate(paperID, .before = 1) %>%
+  mutate(paperID = sample(x = 1:nrow(wosData), size = nrow(wosData), replace = F),
+         excludedReason = NA) %>%
+  relocate(paperID, excludedReason, .before = 1) %>%
   arrange(paperID)
 
 View(wosData)
