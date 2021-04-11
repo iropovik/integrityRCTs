200f25bc0a2c64c29a98f500c85671caf01bc00e
Ivan Ropovik <ivan.ropovik@gmail.com>
2021-04-11 21:22:06 +0200

    basic functions

---
+++ analysis.R
@@ -0,0 +1,197 @@
+#' ---
+#' title: "The prevalence of trauma, PTSD, and other mental health issues: evidence from a representative Slovak sample"
+#' author: "Ivan Ropovik"
+#' date: "`r Sys.Date()`"
+#' output:
+#'    html_document:
+#'       toc: true
+#'       toc_float: true
+#'       code_folding: show
+#'       fig_retina: 2
+#' always_allow_html: yes
+#' ---
+#+ setup, include=FALSE, message=FALSE
+# libraries and settings --------------------------------------------------
+#knitr::opts_chunk$set(echo=FALSE, warning = FALSE)
+
+rm(list = ls())
+
+# install required R libraries if not installed already
+list.of.packages <- c("tidyverse", "magrittr", "devtools", "overlapping", "entropy", "metap", "MBESS", "CarletonStats", "goftest", "qqtest", "beepr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# load required libraries
+lapply(list.of.packages, require, quietly = TRUE, warn.conflicts = FALSE, character.only = TRUE)
+
+# source
+source("functions.R")
+
+# read in the data
+dat <- read_delim("data/data.csv", ";", escape_double = FALSE, col_types = cols(paper = col_character(), sample = col_integer(), variable = col_integer(), group = col_integer(), nClusters = col_integer(), n = col_integer(), mean = col_double(), sd = col_double(), SE = col_double(), decimalM = col_integer(), decimalSD = col_integer(), discreteItemsCount = col_integer(), nPerCellAssumed = col_integer(), notes = col_character()), na = "NA", trim_ws = TRUE)
+# compute SD from SE
+dat <- dat %>%
+  mutate(sd = ifelse(is.na(sd) & !is.na(SE), SE*sqrt(n), sd),
+         varType = 1,
+         studyID = paste(paper, "/", sample, sep = ""),
+         trialID = as.numeric(paper) + as.numeric(sample)/100) %>%
+  group_by(trialID) %>% mutate(id = cur_group_id()) %>% ungroup()
+
+data <- dat %>% select(id, variable, group, n, mean, sd, decimalM, decimalSD, varType, studyID) %>% as.data.frame()
+data <- data[1:100,]
+
+# calculate p-values using Monte Carlo method
+set.seed(1)
+pMC <- sim_distr(m = 10000, data = data, plot_flag = F)
+# nizke p <- priliz rozdielne, vysoke p, priliz rovnake
+
+# calculate p-values using regular ANOVA method
+pAnova <- get_anova_p_vals(data)
+
+# find the matrix value closer to .5
+ps <- matrix(nrow = dim(pMC)[1], ncol = dim(pMC)[2])
+for(i in 1:dim(pMC)[1]){
+  for(j in 1:dim(pMC)[2]){
+ps[i,j] <- ifelse(is.na(pMC[i,j]), NA, c(pMC[i,j], pAnova[i,j])[which.min(abs(c(pMC[i,j], pAnova[i,j]) - .5))])
+  }
+}
+rownames(ps) <- rownames(pMC)
+ps
+
+
+# exclude significant p-values and and rescale the non-significant to (0, 1)
+ps <- ifelse(ps < .05, NA, (ps-0.05)/0.95)
+
+# Analysis based on p-values for individual within-study variables --------
+
+# choose a single p-value for every independent study and permute
+nIterations <- 1000
+pSets <- matrix(nrow = nrow(ps), ncol = nIterations)
+for(i in 1:nIterations){
+  pSets[,i] <- apply(ps, 1, function(x){sample(x[!is.na(x)], size = 1)})
+}
+rownames(pSets) <- rownames(pMC)
+
+# combine the permuted sets of p-values using Stouffer's method
+pSetsStouffer <- apply(pSets, 2, function(x){
+  1 - pnorm(sum(sapply(x, qnorm))/sqrt(length(x)))
+})
+
+# average over the iterations and compute the sd
+(meanStouffer <- c("Mean Stouffer's p over iterations" = mean(pSetsStouffer),
+                  "SD of Stouffer's p over iterations" = sd(pSetsStouffer)))
+
+# Wald's z-test for Stouffer's method
+(waldsStouffer <- c("zWaldStouffer" = zStouffer <- (meanStouffer[1] - .5)/(meanStouffer[2]/sqrt(nIterations)),
+                    "pWaldStouffer" = 2*pnorm(abs(as.numeric(zStouffer)), lower.tail = F)))
+
+# combine the permuted sets of p-values using Fisher's method
+pSetsFisher <- apply(pSets, 2, function(x){
+  pchisq(-2 * sum(log(x)), 2*length(x))
+})
+
+# average over the iterations and compute the sd
+(meanFisher <- c("Mean Stouffer's p over iterations" = mean(pSetsFisher),
+                  "SD of Stouffer's p over iterations" = sd(pSetsFisher)))
+
+# Wald's z-test for Fisher's method
+(waldsFisher <- c("zFisher" = zFisher <- (meanFisher[1] - .5)/(meanFisher[2]/sqrt(nIterations)),
+                  "pFisher" = 2*pnorm(abs(as.numeric(zFisher)), lower.tail = F)))
+
+# Combining the p-values into a study-level p-value -----------------------
+assumedIntercorr <- .2
+pStudyStouffer <- NA
+pStudyFisher <- NA
+for(i in 1:nrow(ps)){# urobit aj korekciu na zaklade effective number of tests
+  corMatrix <- matrix(assumedIntercorr, nrow = sum(!is.na(ps[i,])), ncol = sum(!is.na(ps[i,])))
+  diag(corMatrix) <- 1
+  pStudyStouffer[i] <- stouffer(1 - as.numeric(na.omit(ps[i,])),
+                                R = mvnconv(corMatrix, target = "p", cov2cor = T, side = 2),
+                                adjust = "empirical", size = 100000)$p
+  pStudyFisher[i] <- fisher(1 - as.numeric(na.omit(ps[i,])),
+                                R = mvnconv(corMatrix, target = "p", cov2cor = T, side = 2),
+                                adjust = "empirical", size = 100000)$p
+}
+pStudyStouffer
+pStudyFisher
+
+distrTests <- list(
+  "K-S for Stouffer's p" = ks.test(pStudyStouffer,"punif", 0, 1),
+  "K-S for Fisher's p" = ks.test(pStudyFisher,"punif", 0, 1),
+  "A-D for Stouffer's p" = ad.test(pStudyStouffer, "punif", 0, 1),
+  "A-D for Fisher's p" = ad.test(pStudyFisher, "punif", 0, 1)
+)
+
+qqtest::qqtest(pStudyStouffer, dist = "uniform", legend = T, xlim = c(0, 1), ylim = c(0, 1),
+               xAxisProbs = c(0.2, 0.4, 0.6, 0.8, 1),
+               yAxisProbs = c(0.2, 0.4, 0.6, 0.8, 1),
+               bty = "n", drawPercentiles = T, nreps = 10000)
+
+
+# Distribution overlap ----------------------------------------------------
+
+overlapOut <- NA
+for(i in 1:100){
+  y <- runif(10000, 0, 1)
+  overlapOut[i] <- overlap(list(pStudyStouffer,y), boundaries = list(from = 0, to = 1), plot = F)$OV
+}
+(overlapEst <- c("Overlap estimate mean" = mean(overlapOut),
+                 "Overlap estimate SD" = sd(overlapOut)))
+
+# Overlap density plot
+plotData <- as.data.frame(cbind("pvalues" = c(pStudyStouffer, y), "Distribution" = c(rep(1, length(pStudyStouffer)), rep(2, length(y)))))
+plotData$Distribution <- as.factor(plotData$Distribution)
+ggplot(data = plotData, aes(x = pvalues, group = Distribution, fill = Distribution)) +
+  geom_density(adjust = 0.5, alpha = .5) + theme_classic()
+
+# lack of variability in SDs
+nSim <- 3
+theoreticalSDSD <- theoreticalRangeSD <- empiricalSDSD <- empiricalRangeSD <- data.frame(NA)
+listTheoreticalSDSD <- listTheoreticalRangeSD <- list(NA)
+set.seed(1)
+for(n in 1:nSim){
+  for(i in 1:max(data$id)){
+    for(j in 1:max(data$variable)){
+      d <- data %>% filter(id == i, variable == j)
+      pooledSD <- mean(d$sd)
+      empiricalSDSD[i,j] <- sd(d$sd)
+      empiricalRangeSD[i,j] <- diff(range(d$sd))
+      theoreticalSDSD[i,j] <- d %>% rowwise() %>% transmute(sd(rnorm(n, mean, pooledSD))) %>% unlist() %>% sd()
+      theoreticalRangeSD[i,j] <- d %>% rowwise() %>% transmute(sd(rnorm(n, mean, pooledSD))) %>% unlist() %>% range() %>% diff()
+    }
+  }
+  listTheoreticalSDSD[[n]] <- theoreticalSDSD
+  listTheoreticalRangeSD[[n]] <- theoreticalRangeSD
+}
+
+# average over lists of sdsds and compute the probability of observing equal or smaller degree of variability in SDs
+excessSmallSDs <-  Reduce("+", lapply(listTheoreticalSDSD, function(x){x <= empiricalSDSD}))/nSim
+excessSmallRanges <-  Reduce("+", lapply(listTheoreticalRangeSD, function(x){x <= empiricalRangeSD}))/nSim
+rownames(excessSmallSDs) <- rownames(excessSmallRanges) <- rownames(pMC)
+excessSmallSDs
+excessSmallRanges
+
+
+
+
+######################################
+# Graveyard
+######################################
+
+
+hist(pMC[pMC>.05], breaks = 100)
+hist(pC[pC>.05], breaks = 100)
+
+view(round(as.matrix(pMC, dimnames = NULL), 3))
+view(round(as.matrix(pAnova, dimnames = NULL), 3))
+
+allmetap(beckerp, method = "all")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("EmpiricalBrownsMethod")
+library(EmpiricalBrownsMethod)
+
+# the call to combine p-values
+empiricalBrownsMethod(data_matrix=glypDat, p_values=glypPvals, extra_info=TRUE)
---
+++ functions.R
@@ -0,0 +1,221 @@
+#'@title Assessment of Data Trial Distributions According to the Carlisle-Stouffer Method
+#'
+#'@description Assessment of the distributions of baseline continuous and categorical variables in randomised trials, assuming normal distributions for the former and binomial distributions for the latter. The method used is based on the Carlisle-Stouffer method with Monte Carlo simulations. It calculates p-values for each trial baseline variable, as well as combined p-values for each trial - these p-values measure how compatible are distributions of trials baseline variables with random sampling. This package also allows for graphically plotting the cumulative frequencies of obtained p-values.
+#'
+#'@param m An integer specifying the number of Monte Carlo simulations to be run.
+#'
+#'@param data A dataframe with 9 columns with order and content as specified in the "Details" section.
+#'
+#'@param plot_flag A logical indicating whether cumulative frequencies of p-values are to be plotted in a graph; if TRUE a plot is provided.
+#'
+#'@section Details:
+#'The data must necessarily have 9 columns with the following order and content:
+#'\enumerate{
+#'    \item Trial number: An integer that sequentially identifies the randomised trial from which data concern. Therefore, all rows with data from the first randomised trial should be identified with the number 1 in this first column, data from the second trial should be identified with number 2, etc.
+#'    \item Variable number for each trial: An integer that sequentially identifies the different variables assessed within each trial. Therefore, for each trial, all rows with data from the first reported variable should be identified with the number 1, data from the second variable should be identified with number 2, etc.
+#'    \item Group: An integer that sequentially identifies the different groups of participants for which each variable was assessed.
+#'    \item Number of participants: An integer corresponding to the number of assessed individuals in each group.
+#'    \item Reported mean value (continuous variables) or proportion (categorical variables): A number with the reported result (mean or proportion) obtained for the corresponding variable in each group. Proportions must NOT be presented as percentages.
+#'    \item Reported standard-deviation: A number with the reported standard-deviation obtained for the corresponding variable in each group. This column only concerns continuous variables, and should be left blank for categorical variables.
+#'    \item Number of decimal places: An integer that indicates the number of decimal places to which the corresponding mean or proportion was reported (e.g., 1 for "2.5").
+#'    \item Variable type: 1 for continuous variables; 2 for categorical variables.
+#'    \item Name: A character sequence (e.g., with author name and publication year) that names the randomised trial from which data concern.
+#'    }
+#'The example data - example_trials - provides a model regarding the required organisation and content of the data.
+#'For the sake of a faster testing, the example sets the number of simulations at 100. However, we recommend at least 5000 simulations to be performed in order to obtain more accurate results.
+#'
+#'@return
+#'\enumerate{
+#'\item A data with p-values for each variable in each assessed trial - each trial is displayed in a different row. For each trial, each variable ("V") is displayed in a different column; variables are listed in the same order they are presented in the original data.
+#'\item A data with combined (overall) p-values for each assessed trial - each trial is displayed in a different row.
+#'\item A graph plotting the cumulative frequency of obtained variable p-values (optional).
+#'}
+#'
+#'@examples sim_distr(100,example_trials, TRUE)
+#'
+#'@section Source:
+#'The code for computing p-values for continuous variables is an adaptation of the code provided in AppendixS1 of Carlisle JB and Loadsman JA, 2017, Anaesthesia.
+#'
+#'@section References:
+#'\enumerate{
+#'\item Carlisle JB, Loadsman JA. Evidence for non-random sampling in randomised, controlled trials by Yuhji Saitoh. Anaesthesia 2017;72:17-27
+#'\item Carlisle JB, Dexter F, Pandit JJ, Shafer SL, Yentis SM. Calculating the probability of random sampling for continuous variables in submitted or published randomised controlled trials. Anaesthesia 2015;70:848-858
+#'}
+#'
+#'@importFrom graphics abline par plot
+#'@importFrom stats pnorm qnorm rbinom rnorm
+#'@importFrom utils stack
+#'
+#'@export
+sim_distr <- function(m, data, plot_flag){
+
+  # Name of trials ------------------------------------------------------------
+  naming <- unique(data[,10])
+  data <- data[,c(1:7, 9)]
+  # Compute unbiased sd ------------------------------------------------------------
+  data[,6] <- s.u(data[,6], data[,4])
+  # Number of trials ----------------------------------------------------------
+  max_trial <- max(data[,1])
+
+  # Number of variables on each trial -----------------------------------------
+  max_c1 <- NA
+  for(c in 1:max_trial){
+    c1 <- which(data[,1]==c)
+    max_measure <- max(data[c1,2])
+    max_c1[c] <- max_measure
+  }
+
+  # Definition of a matrix for computation of the p-values for each trial -----
+  ptrialsdf <- matrix(data = NA, nrow = max_trial, ncol = max(data[,2], na.rm = T))
+  for(r in 1:max_trial){
+
+    pvalue_mean <- NA
+    length(pvalue_mean) <- max(data[,2], na.rm = T)
+
+    # Definition for each variable of the current trial... --------------------
+    for(s in 1:max_c1[r]){
+      var_row <- apply(data, 1, function(row) any(row[1]==r & row[2]==s))
+      # Number of rows of the current trial/variable combination --------------
+      var_length <- length(which(var_row))
+      # Lines of the current trial/variable combination -----------------------
+      var_lines <- which(var_row)
+      # Sequence from 1 to the number of rows ("var_sample") ------------------
+      var_sample <- rep(1:var_length)
+      # Sequence from 1 to the number of simulations --------------------------
+      var_diffsample <- rep(1:m)
+      # Sequence from 1 to the number of rows ("var_value") -------------------
+      var_value <- rep(1:var_length)
+      # Sequence from 1 to the number of rows ("var_participants") ------------
+      var_participants <- rep(1:var_length)
+      # sequence from 1 to the number of rows ("var_standard") ----------------
+      var_standard <- rep(1:var_length)
+      # sequence from 1 to the number of rows ("var_type") --------------------
+      var_type <- rep(1:var_length)
+
+      # Statistics from each row of the current trial/variable combination ----
+      for(j in 1:var_length){
+
+        #Sum ------------------------------------------------------------------
+        var_value[j] <- data[var_lines[j],4] * data[var_lines[j],5]
+        # Sample size ---------------------------------------------------------
+        var_participants[j] <- data[var_lines[j],4]
+        # Sum of squares ------------------------------------------------------
+        var_standard[j] <- data[var_lines[j],4] * data[var_lines[j],6]^2
+        # Type of variable (continuous or categorical) ------------------------
+        var_type[j] <- data[var_lines[j],8]
+
+      }
+
+      # Overall statistics for all groups for whom each variable was assessed -
+      # Overall mean --------------------------------------------------------
+      mean_mean <- sum(var_value) / sum(var_participants)
+      # Overall variance ----------------------------------------------------
+      mean_variance <- sum(var_standard) / sum(var_participants)
+      # Overall standard deviation ------------------------------------------
+      mean_sd <- mean_variance^0.5
+      # Mean number of participants among different groups ------------------
+      mean_participants <- sum(var_participants) / var_length
+      # Overall standard error of the mean ----------------------------------
+      se_mean <- mean_sd / sqrt(mean_participants)
+
+      # Sum of the squared differences between group means and the overall mean
+      sum_sqdiff <- sum((data[which(var_row),5] - mean_mean)^2)
+
+      # Simulations of the distributions --------------------------------------
+      mean_sim <- NA
+
+      # Simulations for continuous variables ----------------------------------
+      if (all(var_type==1)){
+
+        # Simulations cycle ---------------------------------------------------
+        for(z in 1:m){
+          mean_sim <- rnorm(1, mean = mean_mean, sd = se_mean)
+          for(i in 1:var_length){
+            var_sample[i] <- round(sum(rnorm(data[var_lines[i],4], mean = mean_sim, sd = mean_sd) / data[var_lines[i],4]), data[var_lines[i],7])
+          }
+          # Overall mean ------------------------------------------------------
+          mean_sample <- sum(var_sample * var_participants) / sum(var_participants)
+          # Squared differences between group means to the overall mean -------
+          sqdiff_sample <- (var_sample - mean_sample)^2
+          # Sum of the squared differences ------------------------------------
+          var_diffsample[z] <- sum(sqdiff_sample)
+        }
+
+        # Simulations for categorical variables ---------------------------------
+      } else {
+
+        # Simulations cycle.
+        for(z in 1:m){
+          mean_sim <- rbinom(1, sum(var_participants), mean_mean) / sum(var_participants)
+          for(i in 1:var_length){
+            var_sample[i] <- round(sum(rbinom(data[var_lines[i],4], 1, mean_sim) / data[var_lines[i],4]), data[var_lines[i],7])
+          }
+          # Overall "mean" ----------------------------------------------------
+          mean_sample <- sum(var_sample * var_participants) / sum(var_participants)
+          # Squared differences between group "means" to the overall "mean" ---
+          sqdiff_sample <- (var_sample - mean_sample)^2
+          # Sum of the squared differences ------------------------------------
+          var_diffsample[z] <- sum(sqdiff_sample)
+        }
+      }
+
+      # Obtention of p-values for each variable -------------------------------
+      pvalue_1 <- sum(var_diffsample < sum_sqdiff) / m
+      pvalue_2 <- sum(var_diffsample <= sum_sqdiff) / m
+      pvalue_mean[s] <- (pvalue_1 + pvalue_2) / 2
+
+    }
+    ptrialsdf[r,] <- 1 - pvalue_mean
+  }
+
+  ptrialsdf <- as.data.frame(ptrialsdf)
+  rownames(ptrialsdf) <- c(naming)
+
+  # Computation of the cumulative frequencies ---------------------------------
+  sdf <- stack(ptrialsdf)
+  sdf <- sort(sdf$values, decreasing=FALSE)
+  sdf2 <- unique(sdf)
+  sdf1 <- table(sdf)
+  sdf1 <- as.data.frame(sdf1)
+  sdf1 <- transform(sdf1, cumFreq = cumsum(Freq), relative = cumsum(Freq) / max(cumsum(Freq)))
+
+  # Plotting cumulative frequencies of p-values -------------------------------
+  if (plot_flag == TRUE){
+    par(pty = "s")
+    plot(sdf2, sdf1$relative, xlab = "P value", ylab = "Cumulative frequency", xlim = c(0,1), ylim = c(0,1))
+    abline(0,1)
+  }
+
+  # Print p-values to the commandline -----------------------------------------
+  return(ptrialsdf)
+} # end of function
+
+# function to calculate p-values from summary statistics using ANOVA
+get_anova_p_vals = function(data){
+  data <- as.data.frame(data)[,-c(9:10)]
+  data[,6] <- s.u(data[,6], data[,4])
+  names(data) = c("trial", "measure", "Group", "number in group", "mean", "sd", "decm", "decsd")
+  MaxC1 <- NA
+  Maxtrial <- max(data$trial, na.rm = T)
+  for(c in 1:Maxtrial){
+    C1 <- which(data[,1]==c)
+    Maxmeasure <- max(data[C1,2], na.rm = T)
+    MaxC1[c] <- Maxmeasure
+  }
+  ANOVAtable2 <- NA
+  ANOVAP2 <- matrix(data=NA,nrow=Maxtrial,ncol=max(data$measure,na.rm=T))
+  for(r in 1:Maxtrial){
+    for(s in 1:MaxC1[r]){
+      Row <- apply(data,1,function(row) any(row[1]==r & row[2]==s, na.rm = T))
+      Lines <- which(Row)
+      # I add this sink statement to avoid printing the results
+      # consider changing to /dev/null
+      sink("carlisle_analysis_temp")
+      ANOVAtable2 <- invisible(anovaSummarized(data[Lines,4],data[Lines,5],data[Lines,6]))
+      sink()
+      ANOVAP2[r,s] <- (as.numeric(ANOVAtable2[8]))
+    }
+  }
+  return(ANOVAP2)
+}
+
